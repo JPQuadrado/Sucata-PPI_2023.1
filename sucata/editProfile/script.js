0886f1669d758bbb57bcc34dc4a15d3e132c854b
Vinicius freitas <89417497+viniciusfre@users.noreply.github.com>
2023-10-06 20:15:41 +0000

    Pending changes exported from your codespace

---
+++ sucata/editProfile/script.js
@@ -0,0 +1,15 @@
+// function popup(){
+//     document.getElementById("popup").style.display = "block";
+
+// }
+
+function popup() {
+    $('#confirmDeleteModal').modal('show');
+  }
+
+  // Add your delete account logic here when the "Sim, excluir" button is clicked
+  $('#deleteAccountButton').click(function() {
+    // You can add your delete account logic here
+    // For now, I'm just closing the modal
+    $('#confirmDeleteModal').modal('hide');
+  });
